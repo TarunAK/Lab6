6f5d40fcb049c276d738e57cc06ccb3fefb1bb20
Hari Govind <grhari1998@gmail.com>
2019-03-01 18:36:15 -0500

    v1.0

--- main.c
+++ main.c
@@ -2,17 +2,59 @@
 #include <stdint.h>
 #include "BSP.h"
 
-void initBuzzerPin(){
-	P2SEL0 &= ~0xf0;
-	P2SEL1 &= ~0xf0;
-	
-	P2DIR |= 0xf0;
-	
+extern uint16_t PWMCycles;
+extern uint32_t SubsystemFrequency;
+uint16_t x, y = 0;
+uint8_t select = 0;
+
+void FindLoc(void){
+	if(x<341){
+		if(y<341){
+			ChangeFreq(1480,(uint16_t)256);
+		}else if(y< 682){
+			ChangeFreq(1175,(uint16_t)256);
+		}else if(y<=1023){
+			ChangeFreq(880,(uint16_t)256);
+		}
+
+	}else if(x<682){
+		if(y<341){
+			ChangeFreq(1661,(uint16_t)256);
+
+		}else if(y< 682){
+			ChangeFreq(880,(uint16_t)0);
+
+		}else if(y<=1023){
+			ChangeFreq(988,(uint16_t)256);
+		}
+
+	}else if(x <= 1023){
+		if(y<341){
+			ChangeFreq(1760,(uint16_t)256);
+
+		}else if(y< 682){
+			ChangeFreq(1319,(uint16_t)256);
+		}else if(y<=1023){
+			ChangeFreq(1109,(uint16_t)256);
+			
+		}
+	}
+}
+
+void ChangeFreq(int freq, uint16_t duty){
+	PWMCycles = SubsystemFrequency/freq;
+  	TA0CCR4 = (duty*PWMCycles)>>10;  // defines when output signal is cleared
+  	TA0CCR0 = PWMCycles - 1;         // defines when output signals are set
+
 }
 
 int main(){
 	WDT_A->CTL = WDT_A_CTL_PW|WDT_A_CTL_HOLD;
-	initBuzzerPin();
 	BSP_Buzzer_Init((uint16_t) 0x00);
-	BSP_Buzzer_Set((uint16_t) 0x200);
+	BSP_Joystick_Init(void);
+	BSP_Buzzer_Set((uint16_t)0);
+	while(1){
+		BSP_Joystick_Input(&x, &y, &select);
+		FindLoc();
+	}
 }
